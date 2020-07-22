579533cf2484d93638db2ef5e5647e2974f68af7
Mark Tolmacs <mark@plukkido.hu>
2020-07-22 20:08:34 +0200

    fix(patch): Make it compatible with Cocoapods 1.9

--- lib/cocoapods-static-swift-framework/patch/static_pod.rb
+++ lib/cocoapods-static-swift-framework/patch/static_pod.rb
@@ -1,26 +1,35 @@
+if Gem::Version.new(Pod::VERSION) < Gem::Version.new('1.9')
+    if Gem::Version.new(Pod::VERSION) < Gem::Version.new('1.7')
 
-if Gem::Version.new(Pod::VERSION) < Gem::Version.new('1.7')
-
-    module Pod
-        class PodTarget
-            def static_framework?
-                return true
-            end
-        end
-    end
-
-else
-
-    # cocoapods 1.7 change the implementation of :static_framework?
-    # The 'BuildType' contain all related things.
-    module Pod
-        class Target
-            class BuildType
-                def linkage
-                    :static
+        module Pod
+            class PodTarget
+                def static_framework?
+                    return true
                 end
             end
         end
-    end
 
+    else
+
+        # cocoapods 1.7 change the implementation of :static_framework?
+        # The 'BuildType' contain all related things.
+        module Pod
+            class Target
+                class BuildType
+                    def linkage
+                        :static
+                    end
+                end
+            end
+        end
+
+    end    
+else
+    module Pod
+        class BuildType
+            def linkage
+                :static
+            end
+        end
+    end
 end
